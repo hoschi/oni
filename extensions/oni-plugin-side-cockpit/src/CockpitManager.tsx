3c09abf5db385ecca2d94babcaff3202221a947a
Stefan Gojan <vollekannehoschi@gmail.com>
2019-08-16 18:30:12 +0200

    Revert "disable/enable GitGutter because of:"
    This reverts commit e8aeb6c5e134376a741f491b3d934cf402be3b37.

--- extensions/oni-plugin-side-cockpit/src/CockpitManager.tsx
+++ extensions/oni-plugin-side-cockpit/src/CockpitManager.tsx
@@ -311,8 +311,6 @@ export class CockpitManager implements Oni.IWindowSplit {
         if (!buffer) {
             throw new Error("Can't find buffer by id: " + bufferId)
         }
-        await this.cockpitEditor.neovim.command(":GitGutterDisable")
-        this.oni.log.info(`sc - disabled Git Gutter`)
         await this.emptyCockpitEditor()
         await this.cockpitEditor.openFile(buffer.filePath, {
             openMode: Oni.FileOpenMode.ExistingTab,
@@ -352,8 +350,6 @@ export class CockpitManager implements Oni.IWindowSplit {
         }
 
         await this.setEditorCursorToState(this.cockpitEditor, activeTab.topLine)
-        await this.cockpitEditor.neovim.command(":GitGutterEnable")
-        this.oni.log.info(`sc - enabled Git Gutter`)
     }
 
     private async setEditorCursorToState(editor: Oni.Editor, topLine: number): Promise<void> {
